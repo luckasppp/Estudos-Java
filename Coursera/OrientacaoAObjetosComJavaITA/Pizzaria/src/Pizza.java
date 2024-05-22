bf832d4f8a522fe44ba4054219c1e4ca38461615
Lucas <lucas_sacul12@hotmail.com>
2024-05-21 21:01:50 -0300

    Início sobre a classe pizza.

---
+++ Coursera/OrientacaoAObjetosComJavaITA/Pizzaria/src/Pizza.java
@@ -0,0 +1,14 @@
+package Coursera.OrientacaoAObjetosComJavaITA.Pizzaria.src;
+
+public class Pizza {
+    public ArrayList<String>
+
+
+    public Pizza() {
+        List<String
+    }
+
+    public void adicionaIngrediente(String ingrediente) {
+        
+    }
+}
