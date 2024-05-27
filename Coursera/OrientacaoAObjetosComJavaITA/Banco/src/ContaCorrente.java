b9e459e5ee1b33be2d79991a9a388d0805c1e55e
Lucas <lucas_sacul12@hotmail.com>
2024-05-27 11:43:45 -0300

    Criação da classe contaCorrente e preparação do ambiente de testes.

---
+++ Coursera/OrientacaoAObjetosComJavaITA/Banco/src/ContaCorrente.java
@@ -0,0 +1,16 @@
+public class ContaCorrente {
+    int saldo;
+
+    public int sacar(int valor) {
+        if (valor > saldo) {
+            return 0;
+        } else {
+            saldo -= valor;
+            return valor;
+        }
+    }
+
+    public void depositar(int valor) {
+        saldo += valor;
+    }
+}
