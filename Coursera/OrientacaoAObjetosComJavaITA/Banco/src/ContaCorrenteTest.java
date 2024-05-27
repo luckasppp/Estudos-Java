693d3dfa58184890949c006c698ffa0273671afc
Lucas <lucas_sacul12@hotmail.com>
2024-05-27 12:19:59 -0300

    melhoria na classe de teste de conta corrente

--- Coursera/OrientacaoAObjetosComJavaITA/Banco/src/ContaCorrenteTest.java
+++ Coursera/OrientacaoAObjetosComJavaITA/Banco/src/ContaCorrenteTest.java
@@ -1,27 +1,31 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+
 public class ContaCorrenteTest {
+    
+    ContaCorrente cc;
+
+    @Before
+    public void setUp() {
+        cc = new ContaCorrente();
+        cc.depositar(200);
+    }
+
     @Test
     public void testDepositar() {
-        ContaCorrente cc = new ContaCorrente();
-        cc.depositar(200);
         assertEquals(cc.saldo, 200);
     }
 
     @Test
     public void testSacar() {
-        ContaCorrente cc = new ContaCorrente();
-        cc.depositar(200);
         cc.sacar(100);
         assertEquals(100, cc.saldo);
     }
 
     @Test
     public void testSacarMaisQueSaldo() {
-        ContaCorrente cc = new ContaCorrente();
-        cc.depositar(200);
         assertEquals(cc.sacar(300), 0);
-        
     }
 }
