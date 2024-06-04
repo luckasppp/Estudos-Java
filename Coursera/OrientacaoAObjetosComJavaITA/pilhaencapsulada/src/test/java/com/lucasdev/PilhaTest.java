45ecb1c127f366b55223abfbc0bb3d8b85185825
Lucas Damasceno <lucas_sacul12@hotmail.com>
2024-06-04 19:08:51 -0300

    criação das classes Pilha e PilhaTest

---
+++ Coursera/OrientacaoAObjetosComJavaITA/pilhaencapsulada/src/test/java/com/lucasdev/PilhaTest.java
@@ -0,0 +1,25 @@
+package com.lucasdev;
+
+import org.junit.jupiter.api.Test;
+
+public class PilhaTest {
+    @Test
+    void testDesemplihar() {
+
+    }
+
+    @Test
+    void testEmpilhar() {
+
+    }
+
+    @Test
+    void testTamanho() {
+
+    }
+
+    @Test
+    void testTopo() {
+
+    }
+}
